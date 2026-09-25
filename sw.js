f57c35d13d3b7615f206e0f40a191f75e9294409
sedatahan <seda.tahan@etat.ge.ch>
2026-09-25 15:47:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── SERVICE WORKER — CAP Candidatures ────────────────────────────────────────
-const CACHE_NAME = 'cap-20260925-1543';
+const CACHE_NAME = 'cap-20260925-1546';
 const ASSETS = ['./'];
 
 self.addEventListener('install', e => {
